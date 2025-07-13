86bc1c4707fe3e8554f755b1cdf9b4b6fec112b5
JitRock22 <jitdas177883@gmail.com>
2025-07-13 21:30:53 +0530

    ...

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -4,15 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD5gIsABMq0jUp0qyTZSt9OfOEw92A-iZ8",
-//   authDomain: "healthgenie-e52a6.firebaseapp.com",
-//   projectId: "healthgenie-e52a6",
-//   storageBucket: "healthgenie-e52a6.firebasestorage.app",
-//   messagingSenderId: "422377768479",
-//   appId: "1:422377768479:web:41c09beb529000804bf72a",
-//   measurementId: "G-BW8SHPV1NG"
-// };
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
